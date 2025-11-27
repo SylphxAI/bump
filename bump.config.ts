c2b23f496a8df53ae2b2ba3fe53fa32a3c02efd3
Kyle Tse <shtse8@gmail.com>
2025-11-27 18:43:50 +0000

    chore: add graduate config for 1.0.0 release

---
+++ bump.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from './src/index.ts'
+
+export default defineConfig({
+	// Graduate from 0.x to 1.0.0 stable release
+	// Remove this after 1.0.0 is published
+	graduate: true,
+})
